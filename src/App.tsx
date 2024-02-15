f466e07743d0535f861eb5350493ae583c1821f1
jvitormelo <jvitor.ovidio2@hotmail.com>
2024-02-15 10:21:29 -0300

    Update dependencies and add voice activation feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useMemo, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import "./App.css";
 import { Gal, gals } from "./data/gals";
+import { create } from "zustand";
 
 function simpleOperatorFilter(
   operator: string,
@@ -36,6 +37,11 @@ function splitOperatorAndValue(input: string) {
 function App() {
   const [height, setHeight] = useState("");
   const [age, setAge] = useState("");
+  const [name, setName] = useState("");
+
+  const { toggleVoiceActive } = useGalsStore((s) => s.actions);
+
+  const [soundDelay, setSoundDelay] = useState(500);
 
   const [showMultipleVersions, setShowMultipleVersions] = useState(true);
 
@@ -61,15 +67,41 @@ function App() {
         console.log(operator);
         return simpleOperatorFilter(operator, gal.parsedAge, value);
       },
+      (gal: Gal) => gal.name.toLowerCase().includes(name.toLowerCase()),
     ];
-  }, [height, showMultipleVersions, age]);
+  }, [height, showMultipleVersions, age, name]);
 
   const filteredGals = useMemo(() => {
     return gals.filter((gal) => activeFilter.every((filter) => filter(gal)));
   }, [activeFilter]);
+
+  function playAllSound() {
+    filteredGals.forEach((gal, i) => {
+      setTimeout(() => {
+        const audio = new Audio(gal.voice);
+
+        audio.play();
+        audio.onplay = () => {
+          toggleVoiceActive(gal.link);
+        };
+        audio.onended = () => {
+          toggleVoiceActive(gal.link);
+        };
+      }, i * soundDelay);
+    });
+  }
+
   return (
     <main>
       <div>
+        <label>
+          Name
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
         <label>
           Height
           <input
@@ -97,6 +129,16 @@ function App() {
             onChange={(e) => setShowMultipleVersions(e.target.checked)}
           />
         </label>
+
+        <label>
+          Next Voice Delay
+          <input
+            value={soundDelay}
+            type="number"
+            onChange={(e) => setSoundDelay(parseInt(e.target.value))}
+          ></input>
+        </label>
+        <button onClick={playAllSound}>BRU ARCHIVUUUU</button>
       </div>
 
       <div
@@ -111,15 +153,81 @@ function App() {
         }}
       >
         {filteredGals.map((gal, i) => (
-          <div key={i} className="hexagon-border">
-            <div className="hexagon">
-              <img width={40} height={45} loading="lazy" src={gal.icon}></img>
-            </div>
-          </div>
+          <GalItem key={i} gal={gal} />
         ))}
       </div>
     </main>
   );
 }
+
+function GalItem({ gal }: { gal: Gal }) {
+  const audio = new Audio(gal.voice);
+
+  const galState = useGalsStore((s) => s.galState.get(gal.link));
+  const intervalRef = useRef(0);
+
+  const playSound = () => {
+    clearTimeout(intervalRef.current);
+    intervalRef.current = setTimeout(() => {
+      audio.play();
+    }, 500);
+  };
+
+  const stopSound = () => {
+    audio.pause();
+    audio.currentTime = 0;
+    clearTimeout(intervalRef.current);
+  };
+
+  return (
+    <div
+      className="hexagon-border"
+      onMouseEnter={playSound}
+      onMouseLeave={stopSound}
+      data-voice-active={galState?.isVoiceActive}
+    >
+      <div className="hexagon">
+        <img
+          className="hexagon-image"
+          width={40}
+          height={45}
+          loading="lazy"
+          src={gal.icon}
+        />
+      </div>
+    </div>
+  );
+}
+
+type GalStore = {
+  galState: Map<
+    string,
+    {
+      isVoiceActive: boolean;
+    }
+  >;
+  actions: {
+    toggleVoiceActive: (name: string) => void;
+  };
+};
+const useGalsStore = create<GalStore>((set) => ({
+  galState: new Map(),
+  actions: {
+    toggleVoiceActive: (name) => {
+      set((state) => {
+        const current = state.galState.get(name);
+        if (!current) {
+          state.galState.set(name, { isVoiceActive: true });
+        } else {
+          state.galState.set(name, { isVoiceActive: !current.isVoiceActive });
+        }
+
+        return {
+          galState: new Map(state.galState),
+        };
+      });
+    },
+  },
+}));
 
 export default App;
--- src/data/gals.ts
+++ src/data/gals.ts
@@ -8,3 +8,11 @@ export const gals = Gals.map((gal) => ({
 }));
 
 export type Gal = (typeof gals)[number];
+
+export const minHeight = Math.min(...gals.map((gal) => gal.parsedHeight));
+
+export const maxHeight = Math.max(...gals.map((gal) => gal.parsedHeight));
+
+export const minAge = Math.min(...gals.map((gal) => gal.parsedAge));
+
+export const maxAge = Math.max(...gals.map((gal) => gal.parsedAge));

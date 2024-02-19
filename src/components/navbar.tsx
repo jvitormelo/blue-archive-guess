c235ca869517fee529927ab4271955f4f0ee286b
jvitormelo <jvitor.ovidio2@hotmail.com>
2024-02-19 14:35:41 -0300

    Add Navbar component to root route

---
+++ src/components/navbar.tsx
@@ -0,0 +1,15 @@
+import { Github } from "lucide-react";
+export const Navbar = () => {
+  return (
+    <header className="flex justify-center py-2 bg-primary">
+      <a
+        href="https://github.com/jvitormelo/blue-archive-guess"
+        target="_blank"
+        rel="noopener noreferrer"
+        className=" text-white rounded-full"
+      >
+        <Github size={24} />
+      </a>
+    </header>
+  );
+};
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
+import { Navbar } from "@/components/navbar";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 // import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <Navbar />
       <main className="max-w-7xl mx-auto lg:p-5 p-3">
         <Outlet />
       </main>
